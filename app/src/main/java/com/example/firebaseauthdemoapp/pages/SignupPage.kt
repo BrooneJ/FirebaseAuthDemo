f7dc8e972943c51f3d7bdc3a37df7168cd4b3e07
HyungwonJin <hwjin92@gmail.com>
2024-09-01 23:26:31 +0900

    feat: add signup functionality on SignupPage

--- app/src/main/java/com/example/firebaseauthdemoapp/pages/SignupPage.kt
+++ app/src/main/java/com/example/firebaseauthdemoapp/pages/SignupPage.kt
@@ -1,5 +1,6 @@
 package com.example.firebaseauthdemoapp.pages
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,15 +11,19 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.firebaseauthdemoapp.AuthState
 import com.example.firebaseauthdemoapp.AuthViewModel
 
 @Composable
@@ -33,6 +38,21 @@ fun SignupPage(
 
     var password by remember {
         mutableStateOf("")
+    }
+
+    val authState = authViewModel.authState.observeAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(authState.value) {
+        when (authState.value) {
+            is AuthState.Authenticated -> navController.navigate("home")
+            is AuthState.Error -> Toast.makeText(
+                context,
+                (authState.value as AuthState.Error).message,
+                Toast.LENGTH_SHORT
+            ).show()
+            else -> Unit
+        }
     }
 
     Column(
@@ -60,7 +80,9 @@ fun SignupPage(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            authViewModel.signup(email, password)
+        }) {
             Text("Create Account")
         }
 
